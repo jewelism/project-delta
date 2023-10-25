6e48392f24f0121d6b1caeace70be8ac021f320e
jungboseok <boseokjung@gmail.com>
2023-10-26 02:04:25 +0900

    feat: forge for build

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // No Node.js APIs are available in this process unless
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+import * as Colyseus from "colyseus.js"; // not necessary if included via <script> tag.
+
+// const Colyseus = require("colyseus.js");
+
+console.log("renderer.ts");
+const client = new Colyseus.Client("ws://localhost:2567");
+
+client
+  .joinOrCreate("room_name")
+  .then((room) => {
+    console.log(room.sessionId, "joined", room.name);
+  })
+  .catch((e) => {
+    console.log("JOIN ERROR", e);
+  });
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+var path = require('path');
+
+module.exports = {
+  watch: false,
+  target: 'electron-renderer',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: `./src/renderer.ts`,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
