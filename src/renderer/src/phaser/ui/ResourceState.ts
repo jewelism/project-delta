baea132d38d69b53e98d3c2ef2461e685e73c4b5
jungboseok <boseokjung@gmail.com>
2023-11-07 05:36:48 +0900

    feat: ResourceState

---
+++ src/renderer/src/phaser/ui/ResourceState.ts
@@ -0,0 +1,42 @@
+import { getUIStyle } from '@/phaser/constants';
+
+export class ResourceState extends Phaser.GameObjects.Text {
+  resourceAmount: number;
+
+  constructor(
+    scene: Phaser.Scene,
+    { x, y, initAmount, texture }: { x: number; y: number; initAmount: number; texture: string },
+  ) {
+    super(scene, x, y, String(initAmount), getUIStyle());
+    this.resourceAmount = initAmount;
+
+    scene.add
+      .image(x - 25, y - 3, texture)
+      .setDisplaySize(20, 20)
+      .setOrigin(0)
+      .setScrollFactor(0)
+      .setDepth(9999);
+
+    const backgroundColor = new Phaser.GameObjects.Rectangle(
+      scene,
+      x - 25,
+      y - 3,
+      20,
+      20,
+      0xffffff,
+    );
+    backgroundColor.setOrigin(0).setScrollFactor(0).setDepth(9998);
+    scene.add.existing(backgroundColor);
+
+    this.setScrollFactor(0).setDepth(9999);
+    scene.add.existing(this);
+  }
+  increase(amount: number) {
+    this.resourceAmount += amount;
+    this.setText(String(this.resourceAmount));
+  }
+  decrease(amount: number) {
+    this.resourceAmount -= amount;
+    this.setText(String(this.resourceAmount));
+  }
+}
